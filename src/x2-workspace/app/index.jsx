6bf96c87d963cd2da558a3ec53496d484111d459
Henry Li <henry1943@163.com>
2018-07-29 15:57:40 +0800

    Add Import component

--- src/x2-workspace/app/index.jsx
+++ src/x2-workspace/app/index.jsx
@@ -1,11 +1,18 @@
 import { hot } from 'react-hot-loader';
 import React from 'react';
 
+import Import from '../components/Import';
+
 import styles from './index.less';
 
 class App extends React.PureComponent {
   render() {
-    return <div className={styles.container}>Hello, X1 Workspace</div>;
+    return (
+      <div className={styles.container}>
+        Hello, X1 Workspace
+        <Import bundleName="x2-home" componentName="App" />
+      </div>
+    );
   }
 }
 
---
+++ src/x2-workspace/components/Import/index.jsx
@@ -0,0 +1,28 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+export default class Import extends React.PureComponent {
+  static propTypes = { bundleName: PropTypes.string.isRequired, componentName: PropTypes.string.isRequired };
+
+  static defaultProps = {};
+
+  state = { Component: null };
+
+  async componentDidMount() {
+    const { bundleName, componentName } = this.props;
+    const bundle = await SystemJS.import(bundleName);
+    const Component = bundle[componentName];
+    if (typeof Component !== 'function') {
+      throw new Error(`Fail to import component. Component '${componentName}' is not found in bundle '${bundleName}'.`);
+    }
+    this.setState({ Component });
+  }
+
+  render() {
+    const { Component } = this.state;
+    if (Component) {
+      return <Component {...this.props} />;
+    }
+    return null;
+  }
+}
